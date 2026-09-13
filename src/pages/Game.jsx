1270f3153ee6d74b65ddaed38fe83a6a4d24680d
VladKhurs <khursvladislav@gmail.com>
2026-09-13 10:35:19 +0300

    fix: make game on full screen

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,7 +4,7 @@ const Game = () => {
       src="/sweet-hell/game/index.html"
       title="My Godot Game"
       /* h-[100dvh] учитывает адресную строку на смартфонах и не дает низу уползать */
-      className="fixed inset-0 w-full h-[100dvh] z-50 border-0 m-0 p-0 bg-black"
+      className="fixed inset-0 w-full h-[80dvh] z-50 border-0 m-0 p-0 bg-black"
       allow="autoplay; fullscreen"
     />
   );
